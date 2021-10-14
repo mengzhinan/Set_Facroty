9eab88f1350a42aa8f19faf826623703f5828559
杜科 <duke@zhihu.com>
2021-10-14 18:11:20 +0800

    init

---
+++ app/src/main/java/com/test/set_factory/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.test.set_factory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+    }
+
+
+
+}
